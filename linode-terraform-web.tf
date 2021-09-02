ee4d2a4fe4eabbe4bfa96ec39ba9603fd3c65b6e
CDW <colin.d.wagner@gmail.com>
2021-09-02 14:12:05 -0500

    new_demo

--- linode-terraform-web.tf
+++ linode-terraform-web.tf
@@ -1,3 +1,4 @@
+# Linode Provider definition
 terraform {
   required_providers {
     linode = {
@@ -8,15 +9,29 @@ terraform {
 }
 
 provider "linode" {
-  token = "token_here"
+  token = var.token
 }
 
+# Example Web Server
 resource "linode_instance" "terraform-web" {
-	image = "linode/ubuntu18.04"
-	label = "Terraform-Web-Example"
-	group = "Terraform"
-	region = "us-east"
-	type = "g6-standard-1"
-	authorized_keys = ["sshkeyhere"]
-	root_pass = "root-kali"
+        image = "linode/centos7"
+        label = "Terraform-Web-Example"
+        group = "Terraform"
+        region = var.region
+        type = "g6-standard-1"
+        swap_size = 1024
+        authorized_keys = [var.authorized_keys]
+        root_pass = var.root_pass
+}
+
+# Example Database Server
+resource "linode_instance" "terraform-db" {
+        image = "linode/ubuntu18.04"
+        label = "Terraform-Db-Example"
+        group = "Terraform"
+        region = var.region
+        type = "g6-standard-1"
+        swap_size = 1024
+        authorized_keys = [var.authorized_keys]
+        root_pass = var.root_pass
 }
