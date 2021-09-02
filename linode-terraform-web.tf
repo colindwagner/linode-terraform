309db5d812d2f4393c86e3417d16a42fd3b9a6dd
CDW <colin.d.wagner@gmail.com>
2021-09-02 13:46:18 -0500

    init2

--- linode-terraform-web.tf
+++ linode-terraform-web.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "linode" {
-  token = "8775c0c0fc540b2eb795d88b8182d72f4d2e9ce126221acadc11f43587d8c24f"
+  token = "token_here"
 }
 
 resource "linode_instance" "terraform-web" {
@@ -17,6 +17,6 @@ resource "linode_instance" "terraform-web" {
 	group = "Terraform"
 	region = "us-east"
 	type = "g6-standard-1"
-	authorized_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfCTgLytFOu89uEG/jzQU7ZkG3eW75X+w8bIJLrlDDdpsrnj9y5EhY7fBr3fNEC9TQwCPgUZrmOtsknXH7CPlk7G8V3I+PqY7KZWq8qZM99YRBeUbFeC5AwXogvH4Nz47RmY2rF5LA7/x/jkkU1d1SnHES7rLyrB8u7jhwUQpp/URh+Beq7MetKRnd3LSJ5a5UJUPM/U67FTEPgwNWd/XwgnzCv7Pg8FQNGKUdxVLD5B/QHtXy1BNiX5v07BCiv1KvPhIREv7VnwjOkslhnLD16xIBbE9e2wCtTGKW3gpL3/8ln13uxlbhvWq/bEYGywhCxCDOVI92TySe8YtWE9WN8XAaZNrC66QqtRztX14KLdSHLLjTPXhmaGm8/13RlDFStNilERxqVaI0HkQ0Q/wEj1nJyjL6KQqMMZjuIe5r2Pcco6MVo0ks5oNRfIYcTKWRZritJh0gedg6oQH1K3BU6Gc/cxV1diWxzoOgGpypmN0AjKHLXW8Wj5f83bmhaDE= root@kali"]
+	authorized_keys = ["sshkeyhere"]
 	root_pass = "root-kali"
 }
